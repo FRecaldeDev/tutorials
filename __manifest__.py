3461207d9448ff81c398f0f8d1e741456a6cd3d6
FRecaldeDev <fiomonsegonzalez@gmail.com>
2025-03-17 09:09:57 -0300

    Inicializacion del modulo realestate

--- __manifest__.py
+++ __manifest__.py
@@ -1,36 +1,22 @@
-# -*- coding: utf-8 -*-
 {
     'name': "Real Estate Advertisement module",
-
     'summary': "Manage and display real estate advertisements, with features such as offers and property details.",
-
     'description': """
 Manage and display real estate advertisements, with features such as offers and property details.""",
-
     'author': "FRecaldeDev",
     'website': "https://www.odoo.com/page/crm",
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
     'category': 'Real Estate',
-    'version': '0.1',
+    'version': '1.0',
 
-    # any module necessary for this one to work correctly
-    'depends': ['base','crm'],
+    'depends': ['base', 'crm'],
 
-    # always loaded
     'data': [
         'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
+        'views/real_estate_property_views.xml',  
     ],
     'installable': True,
     'application': True,
-    
 }
-
---
+++ models/property.py
@@ -0,0 +1,17 @@
+from odoo import models, fields
+
+class RealEstateProperty(models.Model):
+    _name = 'real.estate.property'
+    _description = 'Real Estate Property'
+
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text(string="Description")
+    postcode = fields.Char(string="Postcode")
+    date_availability = fields.Date(string="Available From")
+    expected_price = fields.Float(string="Expected Price", required=True)
+    selling_price = fields.Float(string="Selling Price", readonly=True)
+    bedrooms = fields.Integer(string="Bedrooms")
+    living_area = fields.Integer(string="Living Area (sqm)")
+    facades = fields.Integer(string="Facades")
+    garage = fields.Boolean(string="Garage")
+    garden = fields.Boolean(string="Garden")
